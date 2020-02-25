884b811b190c8daed0eec78e4ce87e9643e8507d
Riza Ardiansyah <rizza.unj@gmail.com>
2020-02-25 17:50:43 +0700

    Navbar Responsive, Logo Responsive, Beranda Done

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -9,49 +9,75 @@
 </head>
 
 <body>
+    <!-- Navbar -->
     @include('templates.navbar')
+
     <!-- Carousel -->
-    <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-            <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-        </ol>
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="{{asset('assets/img/beranda/carousel-1.jpg')}}" class="d-block w-100" alt="img">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>First slide label</h5>
-                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    <div class="carousel">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="{{asset('assets/img/beranda/slide-1.png')}}" class="d-block w-100" alt="img">
+                </div>
+                <div class="carousel-item">
+                    <img src="{{asset('assets/img/beranda/slide-2.png')}}" class="d-block w-100" alt="img">
+                </div>
+                <div class="carousel-item">
+                    <img src="{{asset('assets/img/beranda/slide-3.png')}}" class="d-block w-100" alt="img">
                 </div>
             </div>
-            <div class="carousel-item">
-                <img src="{{asset('assets/img/beranda/carousel-1.jpg')}}" class="d-block w-100" alt="img">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Second slide label</h5>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="{{asset('assets/img/beranda/carousel-1.jpg')}}" class="d-block w-100" alt="img">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Third slide label</h5>
-                    <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </div>
-            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
         </div>
-        <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-        </a>
-        <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-        </a>
     </div>
 
     <!-- Card Berita -->
+    <div class="berita">
+        <h3>Berita</h3>
 
+        <div class="card-group">
+            <div class="card">
+                <img class="card-img-top" src="{{asset('assets/img/beranda/card-1.png')}}" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Pagelaran Konser JYC mengagumkan!</h5>
+                    <p class="card-text">Isinya adalah cuplikan teks bertia yang nantinya ketika diklik akan ke halaman berita secara penuh…
+                    </p>
+                    <p class="card-text"><small class="text-muted">29 Agustus 2019</small></p>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="{{asset('assets/img/beranda/card-2.png')}}" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Bangga! Raih 4 Trofi sekaligus. <br> JYC diundang Gubernur DKI Jakarta</h5>
+                    <p class="card-text">Isinya adalah cuplikan teks bertia yang nantinya ketika diklik akan ke halaman berita secara penuh…
+                    </p>
+                    <p class="card-text"><small class="text-muted">29 Agustus 2019</small></p>
+                </div>
+            </div>
+            <div class="card">
+                <img class="card-img-top" src="{{asset('assets/img/beranda/card-3.png')}}" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">JYC menjadi perwakilan  di Kompetisi Beijing</h5>
+                    <p class="card-text">Isinya adalah cuplikan teks bertia yang nantinya ketika diklik akan ke halaman berita secara penuh… </p>
+                    <p class="card-text"><small class="text-muted">29 Agustus 2019</small></p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    @include('templates.footer')
 </body>
 @include('templates.foot')
 
--- resources/views/templates/navbar.blade.php
+++ resources/views/templates/navbar.blade.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-expand-lg">
+<nav class="navbar navbar-expand-lg navbar-light">
     <div class="container">
-        <a class="navbar-brand" href="#"><img src="{{asset('assets/img/logo-jyc.png')}}" alt="img"></a>
+    <a class="navbar-brand" href="#"><img src="{{asset('assets/img/logo-jyc.png')}}" alt="img"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
             aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
