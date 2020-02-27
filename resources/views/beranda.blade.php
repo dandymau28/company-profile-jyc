d5bb77af0a8d9f94ed38b9a0b168fd4b87dcf64b
Riza Ardiansyah <rizza.unj@gmail.com>
2020-02-27 23:32:55 +0700

    Menyelesaikan Page Tentang Kami, revisi carousel beranda, membuat fixed navbar

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -15,11 +15,6 @@
     <!-- Carousel -->
     <div class="carousel">
         <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
             <div class="carousel-inner">
                 <div class="carousel-item active">
                     <img src="{{asset('assets/img/beranda/slide-1.png')}}" class="d-block w-100" alt="img">
--- resources/views/templates/navbar.blade.php
+++ resources/views/templates/navbar.blade.php
@@ -1,6 +1,6 @@
-<nav class="navbar navbar-expand-lg navbar-light">
+<nav class="navbar navbar-expand-lg navbar-light fixed-top">
     <div class="container">
-    <a class="navbar-brand" href="#"><img src="{{asset('assets/img/logo-jyc.png')}}" alt="img"></a>
+    <a class="navbar-brand" href="{{route('beranda')}}"><img src="{{asset('assets/img/logo-jyc.png')}}" alt="img"></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
             aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
