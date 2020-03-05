e72a4fb06a3edd9b332ef501fdf384616a6c6c88
Amelia Kusuma <ameliakusuma25@gmail.com>
2020-03-05 10:36:11 +0700

    Footer revisi

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -1,74 +1,48 @@
-<footer class="page-footer merah py-3">
+<footer class="page-footer bg-danger pt-3">
     <div class="container">
         <div class="row">
-            <div class="col s12 m3 l3">
-                <h5 class="white-text center-align">Kegiatan</h5>
-                <div class="link-kegiatan .d-none .d-sm-block .d-md-none center-align" style="display:none;">
-                    <a href="#">Latihan Rutin</a> <br>
-                    <a href="#">Daftar Kompetisi</a> <br>
-                    <a href="#">Jakarta National Choir Competition</a>
-                </div>
+            <div class="col-md-4 text-center">
+                <h5 class="text-white">Kegiatan</h5>
+                    <ul class="list-unstyled footer-border">
+                        <li><a class="text-white" href="#!">Latihan Rutin</a></li>
+                        <li><a class="text-white" href="#!">Daftar Kompetisi</a></li>
+                        <li><a class="text-white" href="#!">Jakarta National Choir Competition</a></li>
+                    </ul>
             </div>
-            <div class="col s12 m3 l3">
-                <h5 class="white-text center-align">Berita</h5>
+            <div class="col-md-2 text-center">
+                <h5 class="text-white">Berita</h5>
+                <ul class="list-unstyled footer-border">
+                    <li><a class="text-white" href="#!">Recent</a></li>
+                    <li><a class="text-white" href="#!">Galeri</a></li>
+                    <li><a class="text-white" href="#!">Video</a></li>
+                </ul>
             </div>
-            <div class="col s12 m6 l6">
+            <div class="col-md-3">
+                <div class="mt-5"><a class="text-white" href="#!"><h5>Prestasi</h5></a></div>
+            </div>
+            <div class="col-md-3">
                 <h5 class="white-text right-align">
-                    <img class="social-links" src="{{asset('/assets/img/youtube.png')}}" alt="Youtbe">
-                    <img class="social-links" src="{{asset('/assets/img/email-2.png')}}" alt="Youtbe">
-                    <img class="social-links" src="{{asset('/assets/img/facebook.png')}}" alt="Youtbe">
-                    <img class="social-links" src="{{asset('/assets/img/ig.png')}}" alt="Youtbe">
-                    <img class="social-links" src="{{asset('/assets/img/twitter.png')}}" alt="Youtbe">
+                    <ul class="list-unstyled">
+                        <a class="grey-text text-lighten-3" href="#!"><img class="social-links" src="{{asset('/assets/img/youtube.png')}}" alt="Youtbe"></a>
+                        <a class="grey-text text-lighten-3" href="#!"><img class="social-links" src="{{asset('/assets/img/facebook.png')}}" alt="Fb"></a>
+                        <a class="grey-text text-lighten-3" href="#!"><img class="social-links" src="{{asset('/assets/img/ig.png')}}" alt="Ig"></a>
+                        <a class="grey-text text-lighten-3" href="#!"><img class="social-links" src="{{asset('/assets/img/twitter.png')}}" alt="Twitter"></a>
+                    </ul>
                 </h5>
-            </div>
-        </div>
-        <div class="row .d-sm-none .d-md-block">
-            <div class="col s12 m3 l3 footer-border center-align">
-                <ul>
-                    <li><a class="grey-text text-lighten-3" href="#!">Latihan Rutin</a></li>
-                    <li><a class="grey-text text-lighten-3" href="#!">Daftar Kompetisi</a></li>
-                    <li><a class="grey-text text-lighten-3" href="#!">Jakarta National Choir Competition</a></li>
-                </ul>
-            </div>
-            <div class="col s12 m3 l3 footer-border center-align">
-                <ul>
-                    <li><a class="grey-text text-lighten-3" href="#!">Recent</a></li>
-                    <li><a class="grey-text text-lighten-3" href="#!">Galeri</a></li>
-                    <li><a class="grey-text text-lighten-3" href="#!">Video</a></li>
-                </ul>
-            </div>
-            <div class="col s12 m3 l3 center-align">
-                <ul>
-                    <li><a class="grey-text text-lighten-3" href="#!">Prestasi</a></li>
-                    <li><a class="grey-text text-lighten-3" href="#!"> </a></li>
-                    <li><a class="grey-text text-lighten-3" href="#!"> </a></li>
-                </ul>
-            </div>
-            <div class="col s12 m3 l3 right-align">
                 <div class="row">
-                    <div class="col">
-                        Jl. Jatinegara Timur No.55, RT.11/RW.3, Bali Mester, Kecamatan Jatinegara, Kota Jakarta Timur, Daerah Khusus Ibukota Jakarta 13310
-                    </div>
-                    {{-- <div class="col">
-                        <i class="material-icons">location_on</i>
-                    </div> --}}
+                        <div class="col text-white">
+                            Jl. Jatinegara Timur No.55, RT.11/RW.3, Bali Mester, Kecamatan Jatinegara, Kota Jakarta Timur, Daerah Khusus Ibukota Jakarta 13310
+                        </div>
+                        {{-- <div class="col">
+                            <i class="material-icons">location_on</i>
+                        </div> --}}
                 </div>
-            </div>
-            {{-- <div class="col l4 offset-l2 s12">
-                <h5 class="white-text">Links</h5>
-                <ul>
-                    <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                    <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                    <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                    <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
-                </ul>
-            </div> --}}
-        </div>  
+            </div>  
+        </div>
     </div>
     <div class="footer-copyright">
-        <div class="container">
-            © 2014 Copyright Text
-            <a class="grey-text text-lighten-4 right" href="#!">Dev: Pengembang Nusantara</a>
+        <div class="container text-center">
+            <a class="text-white" href="#!">&copy Dev: Pengembang Nusantara</a>
         </div>
     </div>
 </footer>
