4de83189e6cfc68d91f4e7971fffe939354ee130
dandymau_ <dandymau223@gmail.com>
2020-03-09 10:49:26 +0700

    Controller untuk memberikan data ke halaman beranda

---
+++ app/Http/Controllers/berandaController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+
+class berandaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function beranda()
+    {
+        //3 Berita Terbaru
+        $beritaTerbaru = DB::table('berita')->latest()->take(3)->get();
+
+        //Berita berdasarkan tahun
+        $berita = DB::table('berita')
+            ->where(DB::raw('YEAR(tgl_publish)=2015'))
+            ->orderBy('tgl_publish','desc')
+            ->take(3)
+            ->get();
+
+        //Berita Terbaru untuk Carousel
+        $beritaCarouselTerbaru = DB::table('berita')
+            ->latest()
+            ->take(5)
+            ->get();
+
+        return view('beranda', [
+            'berita' => $beritaTerbaru,
+            'beritaByYear' => $beritaByYear,
+            'beritaCarousel' => $beritaCarouselTerbaru
+            ]);
+    }
+}
