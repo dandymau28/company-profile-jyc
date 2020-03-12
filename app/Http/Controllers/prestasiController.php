59796b2192ac5dc57991626a8e5d8b066843b452
dandymau_ <dandymau223@gmail.com>
2020-03-12 12:20:51 +0700

    pendinamisan page

---
+++ app/Http/Controllers/prestasiController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+
+class prestasiController extends Controller
+{
+    public function prestasi()
+    {
+        $prestasi = DB::table('prestasi')
+            ->latest()
+            ->get();
+
+        return view('prestasi',[
+            'prestasis' => $prestasi,
+            'title' => 'prestasi'
+        ]);
+    }
+}
