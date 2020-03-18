cdd8cd1c8dcf08697ef3db3e2d34c2a0f85be296
dandymau_ <dandymau223@gmail.com>
2020-03-18 18:45:22 +0700

    percontohan membuat pdf untuk mail

--- app/Http/Controllers/mailController.php
+++ app/Http/Controllers/mailController.php
@@ -5,12 +5,17 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 use App\Mail\successMail;
+use PDF;
 
 class mailController extends Controller
 {
-    public function sendEmail()
+    public function sendEmail(Request $request)
     {
-        Mail::to('akunbersama221@gmail.com')
-            ->send(new successMail());
+        $data = ['isi_pesan' => 'Ini PDF'];
+
+        $pdf = PDF::loadView('mail.success',$data);
+        $pdf = $pdf->stream();
+        Mail::to('dandy.maulana99@gmail.com')
+            ->send(new successMail($request, $pdf));
     }
 }
