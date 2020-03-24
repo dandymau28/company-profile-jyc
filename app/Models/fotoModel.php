14e8cb84e2c86baf2a7aee929e63280e11c8a050
dandymau_ <dandymau223@gmail.com>
2020-03-24 17:00:36 +0700

    membuat model untuk foto

--- app/Models/fotoModel.php
+++ app/Models/fotoModel.php
@@ -2,13 +2,16 @@
 
 namespace App\Models;
 
-
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class fotoModel extends Model
+class FotoModel extends Model
 {
-    use SoftDeletes;
-
     protected $table = 'foto';
+    public $timestamps = true;
+    protected $fillable = [
+        'id',
+        'id_kegiatan',
+        'alamat_foto',
+    ];
 }
