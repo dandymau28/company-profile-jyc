2f952be436390db888bef328df05aa54e688a0d4
Swardiantara Silalahi <swardyantara@gmail.com>
2020-04-01 07:16:52 +0700

    detail berita + kategori links

--- resources/views/detailberita.blade.php
+++ resources/views/detailberita.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @include('templates.head')
-    <title>{{$title}} - Jakarta Youth Choir</title>
+    <title>{{Str::title($title)}} - Jakarta Youth Choir</title>
 </head>
 <body>
     {{-- Navbar --}}
@@ -21,49 +21,38 @@
     <!-- Konten -->
     <section id="konten-berita" class="w-80">
         {{-- Berita --}}
-        <div class="row mt-5 d-lg-none">
-            <div class="col">
-                <div class="input-group custom-search-form border border-danger">
-                    <input type="text" class="border-0 form-control" placeholder="Cari berita...">
-                    <span class="input-group-btn">
-                        <button class="btn btn-default" type="button">
-                            <i class="fas fa-search"></i>
-                        </button>
-                    </span>
-                </div>
-            </div>
-        </div>
-
         <div class="row my-5">
             {{-- Card Berita --}}
             <div class="col-lg-8 col-md-12 col-sm-12 mr-md-5">
-                @foreach( $beritas as $berita )
-                    <div  data-aos="zoom-in" class="row mb-4 bg-light text-dark py-3">
-                        <div class="col-12 col-md-6 img-hover-zoom img-hover-zoom--brightness">
-                            <a href="/berita/{{ $berita->slug }}">
-                                <img class="img-fluid" src="{{Storage::url($berita->banner)}}" alt="Gambar Blog">
-                            </a>
-                        </div>
-                        <div class="col-12 col-md-6 pt-sm-3 pt-md-0">
-                            <div class="judul-berita display-5 mb-1">
-                                <a href="/berita/{{ $berita->slug }}">{{ Str::title($berita->judul) }}</a>
-                                
-                            </div>
-                            <div class="tanggal-posting text-muted my-2">
-                                <i class="far fa-clock"></i>
-                                <span>{{ \Carbon\Carbon::parse($berita->tgl_publish)->locale('id')->diffForHumans() }}</span>
-                            </div>
-                            <div class="konten-berita text-justify">
-                                {!! Str::limit(strip_tags($berita->isi_berita), 400) !!}
-                            </div>
-                            <div class="float-right pt-sm-2 pt-md-1">
-                                <a type="button" href="/berita/{{ $berita->slug }}" class="btn btn-outline-secondary">Selengkapnya...</a>
-                            </div>
-                        </div>
+                {{-- Breadcrumb --}}
+                <nav aria-label="breadcrumb" class="mb-5">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="{{ route('beranda') }}">Beranda</a></li>
+                        <li class="breadcrumb-item"><a href="{{ route('berita') }}">Berita</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">{{ Str::title(Str::limit($berita->judul)) }}</li>
+                    </ol>
+                </nav>
+
+                <div id="detail-berita">
+                    <div id="judul-detail-berita" class="border-bottom border-danger">
+                        <h2 class="text-justify">{{ Str::title($berita->judul) }}</h2>
                     </div>
-                @endforeach
-                <div class="row justify-content-center">
-                    {{$beritas->links()}}
+                    <div class="tanggal-posting text-muted my-2">
+                        <i class="far fa-clock"></i>
+                        <span>{{ \Carbon\Carbon::parse($berita->tgl_publish)->locale('id')->diffForHumans() }}</span>
+                    </div>
+                    <div id="banner-berita" class="mt-5">
+                        <img class="img-fluid" src="{{Storage::url($berita->banner)}}" alt="Gambar Blog">
+                    </div>
+                    <div id="isi-detail-berita" class="text-justify my-4">
+                        {!! $berita->isi_berita !!}
+                    </div>
+                    <div id="tag-detail-berita" class="text-white">
+                        <span class="text-muted pr-3"><i class="fas fa-tags fa-2x"></i></span>
+                        <div class="d-inline-flex p-2 hitam-abu border border-secondary">EVENT</div>
+                        <div class="d-inline-flex p-2 hitam-abu border border-secondary">JYC</div>
+                        {{-- <span class="border border-secondary">JYC</span> --}}
+                    </div>
                 </div>
             </div>
             <hr class="d-none d-lg-block">
@@ -205,16 +194,16 @@
                 </div>
                 <hr class="mt-n2">
                 @endforeach --}}
-                <a href="#" class="row justify-content-between mt-2">
+                <div class="row justify-content-between mt-2">
                     <div class="col">
                         <ul>
                             <li>Kompetisi Internasional</li>
                         </ul>
                     </div>
                     <div class="col-1 mr-5">
-                        <span class="">(14)</span>
+                        <span class="badge badge-primary badge-pill">14</span>
                     </div>
-                </a>
+                </div>
                 <hr class="mt-n2">
                 <div class="row justify-content-between mt-2">
                     <div class="col">
