d14be54ee9f5f43511bbdbef6f67450d6c9fa084
Riza Ardiansyah <rizza.unj@gmail.com>
2020-04-02 00:06:02 +0700

    Revisi Tentang Kami

--- resources/views/tentang-kami.blade.php
+++ resources/views/tentang-kami.blade.php
@@ -20,7 +20,7 @@
 
     <div class="container-fluid we-sing">
         <h5>We Sing, We Proud</h5>
-        <p class="container">"Since 2015, Cepo & JYC have been achieving a lot of achievements and awards in many International events such as competition and festival. As the very first competition in 2016, under his directions, JYC got a gold medal in “The 1st Lanna International Choir Competition”, in Thailand. In the following year, JYC was invited to join “The Joy Dancing Beijing & The Cultural and Art Exchange of International Youth” in China. Proudly, JYC always got The Best Performance in in China from approximately 15 participating countries (since 2017- present). Cepo is always motivated and he keeps on encouraging JYC choristers to soar high. In 2017, JYC was taking a challenge to join a choir competition in Indonesia"</p>
+        <p class="container we-sing"><b>"Since 2015, Cepo & JYC</b> have been achieving a lot of achievements and awards in many International events such as competition and festival. As the very first competition in 2016, under his directions, JYC got a gold medal in <b>“The 1st Lanna International Choir Competition”, in Thailand.</b><br><br>In the following year, JYC was invited to join <b>“The Joy Dancing Beijing & The Cultural and Art Exchange of International Youth” in China</b>. Proudly, JYC always got The Best Performance in in China from approximately 15 participating countries (since 2017- present). ,<b>Cepo is always motivated and he keeps on encouraging JYC choristers to soar high. In 2017, JYC was taking a challenge to join a choir competition in Indonesia"</b></p>
     </div>
 
     <div class="jumbotron jumbotron-2 jumbotron-fluid">
