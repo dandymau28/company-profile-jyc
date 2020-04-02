903127ed1a2cde848f640e7498a2720f48563e32
Swardiantara Silalahi <swardyantara@gmail.com>
2020-04-03 03:18:56 +0700

    go to top button on scroll

--- resources/views/templates/foot.blade.php
+++ resources/views/templates/foot.blade.php
@@ -36,4 +36,18 @@
             }
         })
     })
+
+    $(document).ready(function(){ 
+        $(window).scroll(function(){ 
+            if ($(this).scrollTop() > 100) { 
+                $('#scroll').fadeIn(); 
+            } else { 
+                $('#scroll').fadeOut(); 
+            } 
+        }); 
+        $('#scroll').click(function(){ 
+            $("html, body").animate({ scrollTop: 0 }, 600); 
+            return false; 
+        }); 
+    });
 </script>
