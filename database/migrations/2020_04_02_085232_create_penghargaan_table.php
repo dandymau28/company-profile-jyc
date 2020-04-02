f3058472e2c1b017ec782ffb71659bf0a35c9ec8
dandymau_ <dandymau223@gmail.com>
2020-04-02 16:03:42 +0700

    revisi tabel prestasi + tabel penghargaan

---
+++ database/migrations/2020_04_02_085232_create_penghargaan_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePenghargaanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('penghargaan', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('prestasi_id');
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->softDeletes();
+
+            $table->foreign('prestasi_id')->references('id')->on('prestasi');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('penghargaan');
+    }
+}
