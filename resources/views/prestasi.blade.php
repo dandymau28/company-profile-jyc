8af39f7891f440e796ec47b452058dbd57d227da
Swardiantara Silalahi <swardyantara@gmail.com>
2020-04-04 05:24:40 +0700

    card history prestasi

--- resources/views/prestasi.blade.php
+++ resources/views/prestasi.blade.php
@@ -17,17 +17,58 @@
     </div>
 
     <!--Prestasi -->
-    <div class="prestasi container bg-light py-5">
+    <div id="konten-berita" class="prestasi bg-light py-5 w-80">
         <h3 class="mb-5">Prestasi</h3>
+        <div data-aos="zoom-in" class="row justify-content-around">
+            <div class="col-2 col-md-4">
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="{{asset('assets/img/prestasi/1.png')}}" class="d-block card-img-top" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="{{asset('assets/img/prestasi/2.png')}}" class="d-block card-img-top" alt="...">
+                        </div>
+                        <div class="carousel-item">
+                            <img src="{{asset('assets/img/prestasi/3.png')}}" class="d-block card-img-top" alt="...">
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
+            <div class="col-2 col-md-4">
+                <div class="card-body">
+                    {{-- <h5 class="card-title">{{ $prestasi->nama_kompetisi }}</h5> --}}
+                    <h5 class="card-title text-bold">1st Lanna International Choir Competition</h5>
+                    <h5>Chiang Mai, Thailand</h5>
+                    <h5>Oktober 2016</h5>
+                    {{-- <p class="card-text">{{ $prestasi->gelar_juara }}</p> --}}
+                    {{-- <p class="card-text">Penghargaan</p> --}}
+                    <h5 class="card-text"><i class="fas fa-medal text-warning fa-2x pr-2"></i>Gold Medal Folklore Category<i class="fas fa-medal text-warning fa-2x pl-2"></i></h5>
+                </div>
+            </div>
+        </div>
         
-        <div class="card text-center">
+        {{-- <div class="card text-center">
             <div class="card-body">
-                {{-- <h5 class="card-title">{{ $prestasi->nama_kompetisi }}</h5> --}}
+                <h5 class="card-title">{{ $prestasi->nama_kompetisi }}</h5>
                 <h5 class="card-title text-bold">1st Lanna International Choir Competition</h5>
                 <h5>Chiang Mai, Thailand</h5>
                 <h5>Oktober 2016</h5>
-                {{-- <p class="card-text">{{ $prestasi->gelar_juara }}</p> --}}
-                {{-- <p class="card-text">Penghargaan</p> --}}
+                <p class="card-text">{{ $prestasi->gelar_juara }}</p>
+                <p class="card-text">Penghargaan</p>
                 <h5 class="card-text"><i class="fas fa-medal text-warning fa-2x pr-2"></i>Gold Medal Folklore Category<i class="fas fa-medal text-warning fa-2x pl-2"></i></h5>
             </div>
             <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
@@ -59,8 +100,8 @@
             <div class="card-footer">
                 <small class="text-muted">Sumber: Dokumentasi Jakarta Youth Choir</small>
             </div>
-            {{-- <img src="{{asset('assets/img/prestasi/1.png')}}" class="card-img-top" alt="..."> --}}
-        </div>
+            <img src="{{asset('assets/img/prestasi/1.png')}}" class="card-img-top" alt="...">
+        </div> --}}
     </div>
 
 
