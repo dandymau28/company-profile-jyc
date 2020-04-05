4792cca993aca474b97de063a3ebbdc13d56e54e
Swardiantara Silalahi <swardyantara@gmail.com>
2020-04-06 04:32:02 +0700

    sidebar terhangat

--- resources/views/berita.blade.php
+++ resources/views/berita.blade.php
@@ -102,10 +102,6 @@
                     <div class="col">
                         <form id="cari-berita" action="/berita/cari-berita"  method="GET">
                             <div class="input-group custom-search-form border border-danger">
-                            {{-- <select class="border-0 form-control cari-berita">
-                            </select> --}}
-                            {{-- <select name="cari-berita" class="custom-select form-control cari-berita border-0">
-                            </select> --}}
                                 <input name="term" id="term" type="text" class="border-0 form-control" placeholder="Cari berita...">
                                 <span class="input-group-btn">
                                     <button class="btn btn-default" type="submit">
@@ -128,23 +124,18 @@
                 <div class="row mt-2 text-center text-white mx-1">
                     <div class="col py-3">
                         <a class="shadow-lg " href="https://www.facebook.com/jktyouthchoir/"><img class="social-links" src="{{asset('/assets/img/facebook-1.png')}}" alt="Fb"></a>
-                        {{-- <a href="http://"><i id="fb" class="fab fa-facebook-f fa-2x rounded-circle"></i></a> --}}
                     </div>
                     <div class="col py-3">
                         <a class="shadow-lg" href="https://twitter.com/JktYouthChoir"><img class="social-links" src="{{asset('/assets/img/twitter-1.png')}}" alt="Twitter"></a>
-                        {{-- <a href="http://"><i id="twitter" class="fab fa-twitter fa-2x"></i></a> --}}
                     </div>
                     <div class="col py-3">
                         <a class="shadow-lg" href="https://www.youtube.com/channel/UCUelE1nt2PXC_SN6aAfbs8A"><img class="social-links" src="{{asset('/assets/img/youtube-1.png')}}" alt="Youtbe"></a>
-                        {{-- <a href="http://"><i id="youtube" class="fab fa-youtube fa-2x"></i></a> --}}
                     </div>
                     <div class="col py-3">
                         <a class="shadow-lg" href="https://www.instagram.com/jktyouthchoir/"><img class="social-links" src="{{asset('/assets/img/ig-1.png')}}" alt="Ig"></a>
-                        {{-- <a href="http://"><i id="ig" class="fab fa-instagram fa-2x"></i></a> --}}
                     </div>
                     <div class="col py-3">
                         <a class="shadow-lg" href="mailto:jakartayouthchoir@gmail.com"><img class="social-links" src="{{asset('/assets/img/email-2.png')}}" alt="Twitter"></a>
-                        {{-- <a href="http://"><i id="gmail" class="fas fa-envelope fa-2x"></i></i></a> --}}
                     </div>
                 </div>
                 
@@ -156,7 +147,7 @@
                         
                     </div>
                 </div>
-                @foreach( $beritas as $berita )
+                @foreach( $beritaTerhangat as $berita )
                     <div  data-aos="zoom-in" class="row mb-1 text-dark py-3">
                         <div class="col-12 col-md-6">
                             <a href="/berita/{{ $berita->slug }}">
@@ -164,7 +155,7 @@
                             </a>
                         </div>
                         <div class="col-12 col-md-6 pt-sm-3 pt-md-0">
-                            <div class=" mb-1">
+                            <div class=" mb-1 text-justify">
                                 <a href="/berita/{{ $berita->slug }}">{{ Str::title(Str::limit($berita->judul, 80)) }}</a>
                                 
                             </div>
@@ -175,61 +166,6 @@
                     </div>
                     <hr class="my-n2">
                 @endforeach
-                {{-- <div class="row justify-content-between mt-2">
-                    <div class="col-4">
-                        <ul>
-                            <li>2019</li>
-                        </ul>
-                    </div>
-                    <div class="col-1 mr-5">
-                        <span class="badge badge-primary badge-pill">14</span>
-                    </div>
-                </div>
-                <hr class="mt-n2">
-                <div class="row justify-content-between mt-2">
-                    <div class="col-4">
-                        <ul>
-                            <li>2018</li>
-                        </ul>
-                    </div>
-                    <div class="col-1 mr-5">
-                        <span class="badge badge-primary badge-pill">14</span>
-                    </div>
-                </div>
-                <hr class="mt-n2">
-                <div class="row justify-content-between mt-2">
-                    <div class="col-4">
-                        <ul>
-                            <li>2017</li>
-                        </ul>
-                    </div>
-                    <div class="col-1 mr-5">
-                        <span class="badge badge-primary badge-pill">14</span>
-                    </div>
-                </div>
-                <hr class="mt-n2">
-                <div class="row justify-content-between mt-2">
-                    <div class="col-4">
-                        <ul>
-                            <li>2016</li>
-                        </ul>
-                    </div>
-                    <div class="col-1 mr-5">
-                        <span class="badge badge-primary badge-pill">14</span>
-                    </div>
-                </div>
-                <hr class="mt-n2">
-                <div class="row justify-content-between mt-2">
-                    <div class="col-4">
-                        <ul>
-                            <li>2015</li>
-                        </ul>
-                    </div>
-                    <div class="col-1 mr-5">
-                        <span class="badge badge-primary badge-pill">14</span>
-                    </div>
-                </div>
-                <hr class="mt-n2"> --}}
 
                 <div class="row mt-5">
                     <div class="col-5 p-2 text-center text-white merah ml-3">
@@ -241,16 +177,16 @@
                 </div>
                 @foreach($koleksiKategori as $koleksi)
                 <a href="/berita/kategori/{{$koleksi['kategori']}}">
-                <div class="row justify-content-between mt-3">
-                    <div class="col">
-                        <ul>
-                            <li>{{ $koleksi['kategori'] }}</li>
-                        </ul>
+                    <div class="row justify-content-between mt-3">
+                        <div class="col">
+                            <ul>
+                                <li>{{ $koleksi['kategori'] }}</li>
+                            </ul>
+                        </div>
+                        <div class="col-1 mr-5">
+                            <span class="badge badge-pill badge-secondary">{{ $koleksi['hasil'] }}</span>
+                        </div>
                     </div>
-                    <div class="col-1 mr-5">
-                        <span class="badge badge-pill badge-secondary">{{ $koleksi['hasil'] }}</span>
-                    </div>
-                </div>
                 </a>
                 <hr class="mt-n2">
                 @endforeach
