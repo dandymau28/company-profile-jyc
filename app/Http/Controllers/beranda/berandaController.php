fa54eccf0d97ce28e5e425c0738953ec46dfcd12
dandymau_ <dandymau223@gmail.com>
2020-04-06 19:20:55 +0700

    beranda controller tidak dipecah karena hanya satu

---
+++ app/Http/Controllers/beranda/berandaController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use DB;
+class berandaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function beranda()
+    {
+        try {
+            //4 Berita Terbaru
+            $beritaTerbaru = DB::table('berita')->latest()->take(4)->get();
+        } catch (Exception $e) {
+            return $e;
+        }
+
+        if(empty($beritaTerbaru)){
+            $beritaTerbaru = 'Tidak ada berita yang dapat ditampilkan';
+        }
+
+        try {
+            //Berita Terbaru untuk Carousel
+            $beritaCarousel = DB::table('berita')
+                ->where('penting','1')
+                ->latest()
+                ->take(5)
+                ->get();
+        }catch (Exception $e) {
+            return $e;
+        }
+
+        if(empty($beritaCarousel)){
+            $beritaCarousel = 'Tidak ada berita yang dapat ditampilkan';
+        }
+
+        return view('beranda', [
+            'beritas' => $beritaTerbaru,
+            'beritaCarousel' => $beritaCarousel,
+            'title' => 'Beranda',
+            'nav' => 'beranda'
+            ]);
+    }
+}
