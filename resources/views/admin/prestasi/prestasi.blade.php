ee73dbd71e52862ea5095bd8d04b4862a1605f4f
Swardiantara Silalahi <swardyantara@gmail.com>
2020-04-10 07:15:25 +0700

    tampilan + validasi

--- resources/views/admin/prestasi/prestasi.blade.php
+++ resources/views/admin/prestasi/prestasi.blade.php
@@ -25,12 +25,12 @@
     <div class="row">
         <div class="col-md-12">
             <div class="card card-outline card-info">
-                <div class="card-header">
+                {{-- <div class="card-header">
                     <h3 class="card-title">
-                        Bootstrap WYSIHTML5
+                        Tambahkan Prestasi
                         <small>Simple and fast</small>
                     </h3>
-                </div>
+                </div> --}}
                 <!-- /.card-header -->
                 <div class="card-body pad">
                     <form action="{{route('input-prestasi')}}" name="form-prestasi" method="post" enctype="multipart/form-data">
@@ -44,33 +44,47 @@
                             @endphp
                         </div>
                         @endif
-                        <div class="mb-3">
-                            <input type="text" name="nama_kompetisi" id="" placeholder="Nama Kompetisi">
+                        <div class="form-group col-6">
+                            <label for="nama_kompetisi">Nama Kompetisi</label>
+                            <input type="text" id="nama_kompetisi" name="nama_kompetisi" class="form-control" placeholder="Varsovia Cantat" required>
                         </div>
-                        <div class="mb-3">
-                            <input type="text" name="kota" id="" placeholder="Kota">
+                        <div class="form-group col-6">
+                            <label for="kota">Kota</label>
+                            <input type="text" name="kota" id="kota" class="form-control" placeholder="Warsawa" required>
                         </div>
-                        <div class="mb-3">
-                            <input type="text" name="negara" id="" placeholder="Negara">
+                        <div class="form-group col-6">
+                            <label for="negara">Negara</label>
+                            <input type="text" name="negara" id="negara" class="form-control" placeholder="Polandia" required>
                         </div>
-                        <div class="mb-3">
-                            <input type="text" name="tgl_kompetisi" id="" placeholder="Waktu Kompetisi">
+                        <div class="form-group col-6">
+                            <label for="tgl_kompetisi">Waktu Kompetisi</label>
+                            <input type="text" class="form-control" name="tgl_kompetisi" id="tgl_kompetisi" placeholder="November 2019" required>
                         </div>
-                        <div class="mb-3">
-                            <input type="file" name="logo_kompetisi" id="" placeholder="Logo">
+                        <div class="form-group col-6">
+                            <label for="logo_kompetisi">Logo Kompetisi</label>
+                            <input type="file" class="form-control" name="logo_kompetisi" id="logo_kompetisi" placeholder="Logo">
                         </div>
-                        <div class="mb-3">
-                            <input type="file" name="foto_tim" id="" placeholder="Foto Tim">
+                        <div class="form-group col-6">
+                            <label for="foto_tim">Foto Tim</label>
+                            <input type="file" class="form-control" name="foto_tim" id="foto_tim" placeholder="Foto Tim">
                         </div>
-                        <div class="mb-3">
-                            <input type="file" name="foto_piala" id="" placeholder="Foto Piala">
+                        <div class="form-group col-6">
+                            <label for="foto_piala">Foto Piala</label>
+                            <input type="file" class="form-control" name="foto_piala" id="foto_piala" placeholder="Foto Piala">
                         </div>
-                        <div class="mb-3" id="gelar">
-                            <input type="text" name="gelar[]" id="" placeholder="Gelar">
+                        <div class="form-group col-6" id="gelar">
+                            <label for="foto_piala">Penghargaan yang didapat:</label>
+                            <input type="text" class="form-control mb-1 gelar" name="gelar[]" placeholder="Grandprix Winner" required>
                         </div>
-                        <button type="button" onclick="tambah();">Tambah Gelar</button>
+                        <div class="form-group col-6">
+                            <button id="hapus-gelar" type="button"  class="btn btn-info d-none">Hapus Gelar</button>
+                            <button id="tambah-gelar" type="button"  class="btn btn-info">Tambah Gelar</button>
+                        </div>
+                        <div class="col-6 text-right">
+                            <button type="submit" value="post" name="action"  class="btn btn-success">Simpan</button>
+                            {{-- <button type="submit" name="action" value="post">Post</button> --}}
+                        </div>
 
-                        <button type="submit" name="action" value="post">Post</button>
                     </form>
                 </div>
             </div>
@@ -84,16 +98,33 @@
 
 @push('js')
 <script type="text/javascript">
-    function tambah()
-    {
+
+    $('#tambah-gelar').on('click', function () {
+        let parent = $('#gelar');
+        let child = parent.find('gelar');
+        let jum = child.length;
+        $('#hapus-gelar').removeClass('d-none').addClass('d-inline');
         var addChild = document.getElementById('gelar');
-        var tambahElemen = document.createElement('input');
+        let tambahElemen = document.createElement('input');
         tambahElemen.type = 'text';
+        tambahElemen.required = true;
         tambahElemen.name = 'gelar[]';
-        tambahElemen.placeholder = 'Gelar';
-        addChild.appendChild(document.createElement('br'));
+        tambahElemen.placeholder = 'Gold Medal';
+        tambahElemen.className = 'form-control mb-1 gelar';
         addChild.appendChild(tambahElemen);
-    }
+    });
+
+    $('#hapus-gelar').on('click', function () {
+        let parent = $('#gelar');
+        let child = parent.find('.gelar');
+        console.log(child.length);
+        if(child.length > 2) {
+            child.last().remove();
+        } else {
+            child.last().remove();
+            $(this).removeClass('d-inline').addClass('d-none');
+        }
+    });
 
 </script>
 @endpush
