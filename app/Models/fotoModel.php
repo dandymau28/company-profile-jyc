4a453e595458574369e4745d7badd814400f6429
dandymau_ <dandymau223@gmail.com>
2020-05-18 11:23:05 +0700

    up fotomodel yang asli

---
+++ app/Models/fotoModel.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class fotoModel extends Model
+{
+    use SoftDeletes;
+    protected $table = 'foto';
+    
+    protected $fillable = [
+        'id',
+        'deskripsi',
+        'alamat_foto',
+        'kategori'
+    ];
+
+    protected $dates =
+    [
+        'created_at',
+        'updated_at',
+        'deleted_at'
+    ];
+}
